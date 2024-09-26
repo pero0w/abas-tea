4c5d4f1397db50b7ecde9009608836f35fd8a6bc
pero0w <nathantsouza06@gmail.com>
2024-09-26 19:32:10 -0300

    arquivos do projeto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'responsavel',
+    loadChildren: () => import('./responsavel/responsavel.module').then( m => m.ResponsavelPageModule)
+  },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },
+  {
+    path: 'profissional',
+    loadChildren: () => import('./profissional/profissional.module').then( m => m.ProfissionalPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/profissional/profissional.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profissional',
+  templateUrl: './profissional.page.html',
+  styleUrls: ['./profissional.page.scss'],
+})
+export class ProfissionalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
